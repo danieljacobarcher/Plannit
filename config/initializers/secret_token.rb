09a9fa3fef948de6aa5ca22311811aabf4ada6c7
Daniel Smilansky <dsmilansky@gmail.com>
2013-10-27 11:28:35 -0700

    rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Plannit::Application.config.secret_key_base = '4afeefffc7f97bdc2b47753db56577f77e5855897cf453c5913de584af1bba50128ba919588aa728acfad6d05daad8c5913ff37a10725085c53c3ab12301ff88'
